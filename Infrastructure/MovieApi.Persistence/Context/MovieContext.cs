d6e20ca9d2279b89b7f654bbe75a0f661694e1f3
yasin <yasin@LAPTOP-8VQTI9O3>
2025-02-06 17:41:13 +0300

    Add project files.

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-8VQTI9O3\\SQLEXPRESS; initial catalog=ApiMovieDb; integrated security=true; TrustServerCertificate=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+
+        public DbSet<Cast> Casts { get; set; }
+    }
+}
