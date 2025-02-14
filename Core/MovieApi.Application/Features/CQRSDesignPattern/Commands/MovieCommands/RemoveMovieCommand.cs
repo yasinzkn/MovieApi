8806379181cb215f244bd18a8782a86db249aae3
yasin <yasin@LAPTOP-8VQTI9O3>
2025-02-14 12:15:46 +0300

    .

---
+++ Core/MovieApi.Application/Features/CQRSDesignPattern/Commands/MovieCommands/RemoveMovieCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CQRSDesignPattern.Commands.MovieCommands
+{
+    public class RemoveMovieCommand
+    {
+        public int MovieId { get; set; }
+    }
+}
