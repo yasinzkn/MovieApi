3e5016788d21049c2ec00e3950d63cd2f109c7c3
yasin <yasin@LAPTOP-8VQTI9O3>
2025-02-18 14:42:15 +0300

    .

---
+++ Presentation/MovieApi.WebApi/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MovieApi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        //private readonly GetCategory
+    }
+}
