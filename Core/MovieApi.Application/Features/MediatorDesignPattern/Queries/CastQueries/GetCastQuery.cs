19a566625117ae2257bb20d73e4372715626b709
yasin <yasin@LAPTOP-8VQTI9O3>
2025-03-11 12:28:33 +0300

    .

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/CastQueries/GetCastQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.CastQueries
+{
+    public class GetCastQuery: IRequest<List<GetCastQueryResult>>
+    {
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Results/CastResults/GetCastByIdQueryResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Results.CastResults
+{
+    public class GetCastByIdQueryResult
+    {
+        public int CastId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string? Overview { get; set; }
+
+        public string? Biography { get; set; }
+    }
+}
