fc23b9a4dab76fedb16a311d031ce9ff9aeb3f7d
yasin <yasin@LAPTOP-8VQTI9O3>
2025-04-29 12:54:41 +0300

    .

--- Frontends/MovieApi.WebUI/Controllers/MovieController.cs
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -21,13 +21,19 @@ namespace MovieApi.WebUI.Controllers
 
             var client = _httpClientFactory.CreateClient();
             var responseMessage = await client.GetAsync("https://localhost:7060/api/Movies");
-            if(responseMessage.IsSuccessStatusCode)
+            if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultMovieDto>>(jsonData);
                 return View(values);
             }
 
+            return View();
+        }
+
+        public async Task<IActionResult> MovieDetail(int id)
+        {
+            id = 0;
             return View();
         }
     }
