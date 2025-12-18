62282d4ef1d45edf6a77ca210ea713f98f5e1cd1
yasin <yasin@LAPTOP-8VQTI9O3>
2025-12-18 21:30:56 +0300

    .

---
+++ Frontends/MovieApi.WebUI/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
